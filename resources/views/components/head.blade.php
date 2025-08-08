b6f99c6a3bf38290b6dab5814f3b08f6ad52726b
ro749 <45647699+ro749@users.noreply.github.com>
2025-08-08 09:44:28 -0600

    Update head.blade.php

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -8,3 +8,35 @@
     @include('shared-utils::components.scripts')
     <script enum="text/javascript" src="{{ asset('js/options.js') }}"></script>
 @endpush
+@push('scripts')
+<script>
+function formatNumber(value) {
+    if (typeof value !== 'number') return value;
+    return value.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function show(id) {
+    const el = document.getElementById(id);
+    if (el) el.style.display = 'block';
+}
+
+function hide(id) {
+    const el = document.getElementById(id);
+    if (el) el.style.display = 'none';
+}
+</script>
+<script>
+function openPopup(id,time=0){
+    show(id);
+    document.documentElement.style.overflowY ="hidden";
+    if(time!=0){
+        setTimeout(function(){closePopup(id)},time);
+    }
+}
+
+function closePopup(id){
+    hide(id);
+    document.documentElement.style.overflowY ="scroll";
+}
+</script>
+@endpush
