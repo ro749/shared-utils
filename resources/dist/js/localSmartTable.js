f0e43702504fc9239192cd69c4bc0343c3e3aecf
ro749 <45647699+ro749@users.noreply.github.com>
2025-08-22 11:43:05 -0600

    Add dynamic action buttons to localSmartTable
    Appends a column with configurable action buttons to the table. Also adds event handlers for row deletion and saving table data. Removes test data and prepares the table for dynamic data input.

--- resources/dist/js/localSmartTable.js
+++ resources/dist/js/localSmartTable.js
@@ -16,12 +16,32 @@
             let column = { data: col };
             columns.push(column);
         }
-        console.log(columns);
+        columns.push({
+            data: null,
+            render: function (data, type, row) {
+                var buttons = '';
+                for(var button in options.buttons){
+                    buttons += `
+                        <button type="button" class="btn `+options.buttons[button].button_class+` w-32-px h-32-px `+options.buttons[button].background_color_class+' '+options.buttons[button].text_color_class+` rounded-circle d-inline-flex align-items-center justify-content-center">
+                            <iconify-icon icon="`+options.buttons[button].icon+`"> 
+                            </iconify-icon>
+                        </button>
+                    `;
+                }
+                return buttons;
+            }
+        });
+        
         var table = $table.DataTable({
             columns: columns,
-            data: [{
-                "name": "test"
-            }],
+            data: [],
+        });
+
+        $table.on('click', '.delete-btn', function(event) {
+            table.row($(this).parents('tr')).remove().draw();
+        });
+        $('#save-'+options.id).on('click', function() {
+            console.log(table.data().toArray());
         });
     };
 })(jQuery);
