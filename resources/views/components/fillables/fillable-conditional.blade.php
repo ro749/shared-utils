04b5fb878a74162a5274fd5712105204259a6ea3
ro749 <45647699+ro749@users.noreply.github.com>
2025-09-29 14:46:16 -0600

    Update fillable-conditional.blade.php

--- resources/views/components/fillables/fillable-conditional.blade.php
+++ resources/views/components/fillables/fillable-conditional.blade.php
@@ -1,4 +1,5 @@
-@if(empty($unit) || (!empty($unit) && !empty($unit->{$id})))
+@props(['unit' => null, 'id' => ''])
+@if(empty($unit) || (!empty($unit) && !empty($unit->{$id}) && $unit->{$id} != 0))
 <div id="if-{{ $id }}" {{ $attributes }}>
     {{ $slot }}
 </div>
