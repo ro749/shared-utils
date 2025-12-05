c6bb09e4f10e55abf9f908cd3e9fbda5a43eb2d8
ro749 <45647699+ro749@users.noreply.github.com>
2025-12-05 12:21:29 -0600

    Update data.blade.php

--- resources/views/components/data.blade.php
+++ resources/views/components/data.blade.php
@@ -1,13 +1,25 @@
 @props(['data' => null])
-<div x-data="{}" {{ $attributes }}>
+<div x-data="{}" id="{{ $data->get_id() }}" {{ $attributes }}>
     {{ $slot }}
 </div>
 
 @if($data->dynamic)
 @push('scripts')
-    <script>
-        
-    </script>
+<script>
+function get_{{ $data->get_id() }}() {
+    $.ajax({
+        url: "/data/{{ $data->get_id() }}",
+        method: "GET",
+        success: function (data) {
+            Alpine.$data($('#{{ $data->get_id() }}')[0]).data = data;
+        }
+    });
+}
+get_{{ $data->get_id() }}();
+</script>
+@endpush
+@push('reset')
+get_{{ $data->get_id() }}();
 @endpush
 @endif
     
