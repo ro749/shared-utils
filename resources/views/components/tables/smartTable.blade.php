8c73a5b81f81f958b0475d8e075d76210145f4ce
ro749 <45647699+ro749@users.noreply.github.com>
2025-12-29 13:30:32 -0600

    Update smartTable.blade.php

--- resources/views/components/tables/smartTable.blade.php
+++ resources/views/components/tables/smartTable.blade.php
@@ -1,4 +1,4 @@
-<table id="{{ $table->get_id() }}" class="table bordered-table mb-0" style ="width:{{ 2500/36 }}% !important;">
+<table id="{{ $table->get_id() }}" class="table bordered-table mb-0" style ="width:{{ 3500/36 }}% !important;">
     <thead>
         <tr id="{{ $table->get_id() }}-header">
             @foreach($table->get_columns() as $key=>$column)
