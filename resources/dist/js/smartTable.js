b3e4eee4d6099fd787bbcdac3346757cb317fbbb
ro749 <45647699+ro749@users.noreply.github.com>
2026-02-02 11:09:12 -0600

    Update smartTable.js

--- resources/dist/js/smartTable.js
+++ resources/dist/js/smartTable.js
@@ -73,6 +73,15 @@
                             return `${m}/${d}/${y}`;
                         }
                         break;
+                    case 'time_since':
+                        column.render = (data) => {
+                            let value = renderFn ? renderFn(data) : data;
+                            if(value == null || value === ''){
+                                return 'nunca';
+                            }
+                            return timeAgo(value);
+                        }
+                        break;
                     case '':
                     case null:
                         column.render = (data) => {
@@ -493,4 +502,26 @@
             }
         });
     };
+
+    const rtf = new Intl.RelativeTimeFormat('es', { numeric: 'auto' });
+
+    function timeAgo(date) {
+        const seconds = Math.floor((new Date() - new Date(date)) / 1000);
+        const intervals = {
+            year: 31536000,
+            month: 2592000,
+            week: 604800,
+            day: 86400,
+            hour: 3600,
+            minute: 60
+        };
+        
+        for (let [unit, secondsInUnit] of Object.entries(intervals)) {
+            const interval = Math.floor(seconds / secondsInUnit);
+            if (interval >= 1) {
+                return rtf.format(-interval, unit);
+            }
+        }
+        return 'hace un momento';
+    }
 })(jQuery);
