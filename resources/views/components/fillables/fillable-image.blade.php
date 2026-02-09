b97324fd90425049c888eefe13e44595169e7daa
ro749 <45647699+ro749@users.noreply.github.com>
2026-02-09 16:22:32 -0600

    Update fillable-image.blade.php

--- resources/views/components/fillables/fillable-image.blade.php
+++ resources/views/components/fillables/fillable-image.blade.php
@@ -1,12 +1,13 @@
 @props(['id' => '', 'data' => '', 'src' => '', 'ext'=>'', 'unit'=>null])
 
+@if(!empty($unit))
+
 @php
 if(is_array($unit->{$data}) && count($unit->{$data}) == 1){
   $unit->{$data} = $unit->{$data}[0];
 }
 @endphp
 
-@if(!empty($unit))
 @if(is_string($unit->{$data}))
 <img class="image-{{ $id }}" src='{{ config('filesystems.disks.external.url', '').$src.$unit->{$data}.$ext }}' {{ $attributes }}>
 @else
@@ -19,8 +20,34 @@ if(is_array($unit->{$data}) && count($unit->{$data}) == 1){
 
 @endif
 
+
 @if(empty($unit))
+<div id='{{ $id }}'></div>
 @push('fill')
-    $('.image-{{ $id }}').attr('src', '{{ config('filesystems.disks.external.url', '').$src }}'+data['{{ $data }}']+'{{ $ext }}');
+    $('#{{ $id }}').empty();
+    if(Array.isArray(data['{{ $data }}'])){
+      if(data['{{ $data }}'].length == 1){
+        data['{{ $data }}'] = data['{{ $data }}'][0];
+      }
+    }
+    if(Array.isArray(data['{{ $data }}'])){
+      var html_text = '<div class="owl-carousel owl-theme owl-single-dots">';
+      for(var i = 0; i < data['{{ $data }}'].length; i++){
+        var image_html_text = data['{{ $data }}'][i];
+        var image_url = '{{ config('filesystems.disks.external.url', '').$src }}'+image_html_text+'{{ $ext }}';
+        html_text += '<img class="image-{{ $id }}" src="'+image_url+'" {{ $attributes }}>';
+      }
+      html_text += '</div>';
+      $('#{{ $id }}').html(html_text);
+      $('.owl-single-dots').owlCarousel({
+          loop:true,
+          items: 1,
+          nav:false,
+          dots:true,
+      });
+    }
+    else{
+      $('#{{ $id }}').html('<img class="image-{{ $id }}" src="{{ config('filesystems.disks.external.url', '').$src }}'+data['{{ $data }}']+'{{ $ext }}" {{ $attributes }}>');
+    }
 @endpush
 @endif
