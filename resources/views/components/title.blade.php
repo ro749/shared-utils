5f5ed1651f2a3184018775c88e27fe9e2e6264dc
ro749 <45647699+ro749@users.noreply.github.com>
2026-03-03 14:50:18 -0600

    Update title.blade.php

--- resources/views/components/title.blade.php
+++ resources/views/components/title.blade.php
@@ -1,4 +1,6 @@
 <title>{{ config('app.name', 'Laravel') }}</title>
 @if(!empty(config('app.icon')))
 <link rel="icon" href="{{ config('app.icon') }}" type="image/gif" sizes="16x16">
+@else
+<link rel="icon" href="data:,">
 @endif
