ff6efa8d9ac44b162f28d056556a77b4de8a34b6
Xavota <57270722+Xavota@users.noreply.github.com>
2026-03-25 17:55:34 -0600

    Update OverrideFile.php

--- src/Commands/OverrideFile.php
+++ src/Commands/OverrideFile.php
@@ -19,12 +19,20 @@ class OverrideFile extends Command
         $file = $this->argument('file');
         $file_override = config('overrides.'.$type.'s.'.$file);
         $route_parts = explode('\\', $file_override);
+        $packageNK = $route_parts[1];
         $route_parts[1] = Str::kebab($route_parts[1]);
         $package = $route_parts[1];
         $route = implode('/', array_slice($route_parts, 2));
 
         $content = File::get(path: base_path('../'.$package.'/src/'.$route.'.php'));
         $content = preg_replace('/namespace Ro749\\\\.*\\\\/', 'namespace App\\', $content);
+
+        $matches = [];
+        preg_match('/class\s+(\w+)\s+extends\s+(\w+)/', $content, $matches);
+        $parent_class = $matches[2];
+        $content = preg_replace('/use (.*\\\\)*(.*)\\\\'.$parent_class.';/', 'use Ro749\\\\'.$packageNK.'\\\\$2\\\\'.$file.' as '.$parent_class.';', $content);
+        $content = preg_replace('/parent::__construct\((((.*)\s)*)\);/', 'parent::__construct();', $content);
+
         $filePath = app_path($route.'.php');
         if (!file_exists(dirname($filePath))) {
             mkdir(dirname($filePath), 0777, true);
