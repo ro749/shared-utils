86ee31aec25f59d93f118e9f48906c1fa1813aed
Xavota <57270722+Xavota@users.noreply.github.com>
2026-03-26 16:05:07 -0600

    Override File now ask for types and file

--- src/Commands/OverrideFile.php
+++ src/Commands/OverrideFile.php
@@ -3,13 +3,17 @@
 namespace Ro749\SharedUtils\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Console\PromptsForMissingInput;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
-class OverrideFile extends Command
+use function Laravel\Prompts\search;
+use function Laravel\Prompts\select;
+
+class OverrideFile extends Command implements PromptsForMissingInput
 {
-    protected $signature = 'override:file {type} {file}';
+    protected $signature = 'override:file {type?} {file?}';
 
     protected $description = 'Copies a file from the template to the project';
 
@@ -17,8 +21,69 @@ class OverrideFile extends Command
     {
         $type = $this->argument('type');
         $file = $this->argument('file');
+
+        $allConfigs = config('overrides');
+        unset($allConfigs["views"]);
+        unset($allConfigs["image_map_pro"]);
+        unset($allConfigs["sender"]);
+        $configTypes = array_keys($allConfigs);
+
+        if (!$type || !isset($allConfigs[$type])) {
+            $this->info("Type missing or non-existent.");
+
+            $choice = windows_os()
+            ? select(
+                "Choose a valid type to override",
+                $configTypes,
+                scroll: 15,
+            )
+            : search(
+                label: "Choose a valid type to override",
+                placeholder: 'Search...',
+                options: fn ($search) => array_values(array_filter(
+                    $configTypes,
+                    fn ($choice) => str_contains(strtolower($choice), strtolower($search))
+                )),
+                scroll: 15,
+            );
+
+            $type = $choice;
+        }
+
+        if (!$file || !isset($allConfigs[$type][$file])) {
+            $this->info("File missing or non-existent.");
+
+            $configFiles = array_keys($allConfigs[$type]);
+
+            $choice = windows_os()
+            ? select(
+                "Choose a valid file to override",
+                $configFiles,
+                scroll: 15,
+            )
+            : search(
+                label: "Choose a file type to override",
+                placeholder: 'Search...',
+                options: fn ($search) => array_values(array_filter(
+                    $configFiles,
+                    fn ($choice) => str_contains(strtolower($choice), strtolower($search))
+                )),
+                scroll: 15,
+            );
+
+            $file = $choice;
+        }
+        
+        $this->info("Overriding $type -> $file");
+
+        if (str_ends_with($type, 's')) {
+            $type = substr($type, 0, -1);
+        }
+
         $file_override = config('overrides.'.$type.'s.'.$file);
         $route_parts = explode('\\', $file_override);
+        $this->info("route parts: ".json_encode($route_parts, JSON_PRETTY_PRINT));
+
         $packageNK = $route_parts[1];
         $route_parts[1] = Str::kebab($route_parts[1]);
         $package = $route_parts[1];
@@ -39,5 +104,12 @@ class OverrideFile extends Command
         }
         File::put(app_path($route.'.php'), $content);
         $this->call('generate:overrides');
+    }
+
+    protected function promptForMissingArgumentsUsing(): array
+    {
+        return [
+            'type' => ['What type of data are you overiding?', "E.g. Table"],
+        ];
     }
 }
