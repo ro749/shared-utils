6e1b924a9ce863d2eb74dd684a5d2a0ab90ff21a
Xavota <57270722+Xavota@users.noreply.github.com>
2026-03-26 18:09:24 -0600

    insert between keys function for arrays

--- src/Helpers.php
+++ src/Helpers.php
@@ -3,3 +3,29 @@ function image($path)
 {
     return str_replace(' ', '%20', Storage::disk('external')->url($path));
 }
+
+function insert_between_keys($array, $after_key, $new_key, $new_value) {
+    $keys = array_keys($array);
+    // Find the position (index) of the key we want to insert after
+    $position = array_search($after_key, $keys);
+
+    // If the key exists, increment position to insert *after* it
+    if ($position !== false) {
+        $position += 1;
+    } else {
+        // If not found, add to the end
+        $position = count($array);
+    }
+
+    // Split the array into two parts
+    $array_part1 = array_slice($array, 0, $position, true); // Retain keys
+    $array_part2 = array_slice($array, $position, null, true); // Retain keys
+
+    // Create the new key-value pair as an array
+    $new_pair = [$new_key => $new_value];
+
+    // Combine the parts with the new pair in the middle using the union operator (+)
+    $new_array = $array_part1 + $new_pair + $array_part2;
+
+    return $new_array;
+}
