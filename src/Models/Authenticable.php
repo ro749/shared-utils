639c6cdddf3438546c2b0531d75d278e9f1fc63d
ro749 <45647699+ro749@users.noreply.github.com>
2026-04-27 17:38:31 -0600

    Update Authenticable.php

--- src/Models/Authenticable.php
+++ src/Models/Authenticable.php
@@ -5,7 +5,7 @@ namespace Ro749\SharedUtils\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 class Authenticable extends Authenticatable
 {
-    public static function instance(): Model
+    public static function instance(): Authenticatable
     {
         $basename = class_basename(static::class);
         return new (config('overrides.models.'.$basename));
