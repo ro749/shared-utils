e262984121a4b2312faf6298d6154ea15f982f3a
ro749 <45647699+ro749@users.noreply.github.com>
2026-09-10 13:25:56 -0600

    changes

--- src/Forms/ImageUploader.php
+++ src/Forms/ImageUploader.php
@@ -6,12 +6,10 @@ class ImageUploader extends Field
 {
     public string $component = 'sharedutils::image-uploader';
     public string $route = '';
-    public string $view = '';
-    public array $view_data = [];
-
     public string $name;
     public string $data;
     public string $class;
+    public string $imageUrl;
 
     public function __construct(
         string $route,
@@ -19,25 +17,20 @@ class ImageUploader extends Field
         bool $autosave = false,
         string $name = "",
         string $data = "",
-        string $class = "")
+        string $class = "",
+        string $imageUrl = ""
+    )
     {
         parent::__construct(InputType::IMAGE, label: $label, autosave: $autosave);
         $this->route = $route;
         $this->name = $name;
         $this->data = $data;
         $this->class = $class;
+        $this->imageUrl = $imageUrl;
     }
 
     public static function getType(): string
     {
         return 'image';
-    }
-
-    public function render($name="")
-    {
-        return view('shared-utils::components.forms.image-uploader', [
-            'element' => $this,
-            'name' => $this->name,
-        ]);
     }
 }
